634c1a136d13ee7fc914d3062461c1e3091748ad
Ben Szuchmacher <59138522+bszuchmacher@users.noreply.github.com>
2021-05-05 10:55:19 +0300

    final fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,13 +10,16 @@ function Header() {
 				<img className="header__logo" src={logo} alt="logo" />
 			</Link>
 			<div className="header__right">
-				<div style={{ paddingRight:200,width:900}}>
+				<div style={{ paddingRight:300,width:1200}}>
 				<input placeholder="search" type="text" class="ant-input" value=""></input>
 				</div>
 				<div className="header__right__auth">
 					<Link to="/auth" className="header__right__login">
 						Log In
 					</Link>
+					{/* <Link to="/feed" className="header__right__signup">
+						See Your Feed
+					</Link> */}
 				</div>
 			</div>
 		</header>
